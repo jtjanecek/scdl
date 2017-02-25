49052d95799eb75622bdb8c439aa495ea618ca68
mrwnwttk <mrwnwttk@mrwnwttks-MacBook-Air.local>
2017-02-25 21:07:56 +0100

    Rewrote script, lots of bug fixes

--- scdl.py
+++ scdl.py
@@ -1,3 +1,5 @@
+#scdl_rewrite
+# -*- coding: utf-8 -*-
 import sys
 import string
 import requests
@@ -7,48 +9,106 @@ import re
 import os
 import os.path
 import mutagen
+import shutil
+import tempfile
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC
 
-soundcloud_url = raw_input("Please enter a URL: ")
+def main():
+	#get url from user
+	soundcloud_url = unicode(raw_input("Please enter a URL:"))
+	link_type, soundcloud_url = link_detection(soundcloud_url)
+	hide_cursor()
+	try:
+		if link_type == 1:
+			download_single_track(soundcloud_url)
+			show_cursor()
+		elif link_type == 2:
+			download_playlist(soundcloud_url)
+			show_cursor()
+		elif link_type == 3:
+			download_user_tracks(soundcloud_url)
+			show_cursor()
+		#elif link_type == 4:
+		# TODO - ADD LIKES
+		else:
+			show_cursor()
+	except:		
+		show_cursor()
 
-def get_trackid_single_song(soundcloud_url):
-	client_id = "fDoItMDbsbZz8dY16ZzARCZmzgHBPotA"
-	resolve_url = "https://api.soundcloud.com/resolve.json?url="
-	s = requests.get(resolve_url + soundcloud_url + "&client_id=" + client_id)
-	s = s.content
-	trackid = json.loads(s)
-	track_name = trackid["title"]
-	print track_name
-	trackid = str(trackid["id"])
-	print "Track ID: " + trackid
+def link_detection(soundcloud_url):
+	#Decides the type of link which was entered
+	# 1 = single track
+	# 2 = playlist
+	# 3 = tracks of a user
+	# 4 = likes of a user
 
-	return track_name, trackid
+	#Fix a missing https://
+	https_regex = re.compile(r"^https:\/\/soundcloud\.com.{1,}$")
+	if not (https_regex.match(soundcloud_url)):
+		soundcloud_url = "{}{}".format(u"https://", soundcloud_url)
 
-def get_trackids_playlist(soundcloud_url):
+	#Fix URL if it is a single track out of a playlist
+	if '?in=' in soundcloud_url:
+		soundcloud_url = soundcloud_url.split('?in=')
+		soundcloud_url = soundcloud_url[0]
 
-	#Get playlist URL into the right format
-	soundcloud_url = soundcloud_url.split("https://")
-	soundcloud_url = soundcloud_url[1]
+	#URL is a playlist
+	if '/sets' in soundcloud_url:
+		return 2, soundcloud_url
 
-	client_id = "c8ce5cbca9160b790311f06638a61037"
-	app_version = "1481130054"
-	resolve_url = "https://api-mobi.soundcloud.com/resolve?permalink_url=https%3A//" + soundcloud_url + "&client_id=" + client_id + "&format=json&app_version=" + app_version
+	#URL for tracks of a user
+	user_re_tracks_1 = re.compile(r"^https:\/\/soundcloud.com\/[abcdefghijklmnopqrstuvwxyz\-_1234567890]{1,}$")
+	user_re_tracks_2 = re.compile(r"^https:\/\/soundcloud.com\/[abcdefghijklmnopqrstuvwxyz\-_1234567890]{1,25}\/tracks$")
+
+	if (user_re_tracks_1.match(soundcloud_url)) or (user_re_tracks_2.match(soundcloud_url)):
+		return 3, soundcloud_url
+
+	#URL for likes of a user
+	user_re_likes = re.compile(r"^https:\/\/soundcloud.com\/[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\-_1234567890]{1,}\/likes$")
+	if (user_re_likes.match(soundcloud_url)):
+		return 4, soundcloud_url
+
+	#This is a single track
+	else:
+		return 1, soundcloud_url
+
+def hide_cursor():
+	sys.stdout.write("\033[?25l")
+	sys.stdout.flush()
+
+def show_cursor():
+		sys.stdout.write("\033[?25h")
+		sys.stdout.flush()
+
+def download_single_track(soundcloud_url):
+	trackid = get_track_id(soundcloud_url)
+	print "Track ID:", trackid
+	track_name, artist, coverflag, cover_file = get_tags(soundcloud_url)
+	download_track(trackid, soundcloud_url, track_name)
+	add_tags(track_name, artist, cover_file, 0)
+	print "\nDone!"
+
+def get_playlist_id(soundcloud_url):
+	client_id = u"c8ce5cbca9160b790311f06638a61037"
+	app_version = u"1481130054"
+	resolve_url = u"https://api-mobi.soundcloud.com/resolve?permalink_url=" + soundcloud_url + u"&client_id=" + client_id + u"&format=json&app_version=" + app_version
 	playlist_id_request = requests.get(resolve_url)
 
 	# Get the Playlist ID
 	playlist_id_request = playlist_id_request.content
-	playlist_id_request = json.loads(str(playlist_id_request))
-	playlist_id = str(playlist_id_request["id"])
-	print "Playlist ID: ", playlist_id_request["id"]
+	playlist_id_request = json.loads(playlist_id_request)
+	return unicode(playlist_id_request["id"])
 
-	# Get the Permalink URLs for each track and put them into a list
-
-	playlist_api_url = "http://api.soundcloud.com/playlists/" + playlist_id + "?client_id=" + client_id
+def get_playlist_tracks(playlist_id):
+	client_id = u"c8ce5cbca9160b790311f06638a61037"
+	playlist_api_url = u"http://api.soundcloud.com/playlists/" + playlist_id + u"?client_id=" + client_id
+	print playlist_api_url
 	playlist_urls = requests.get(playlist_api_url)
 	permalink_urls = json.loads(playlist_urls.content)
-	#print "Amount of tracks in the playlist:", len(permalink_urls["tracks"])
+
+	album = permalink_urls["title"]
 
 	permalink_url = []
 	trackid = []
@@ -61,11 +121,66 @@ def get_trackids_playlist(soundcloud_url):
 		permalink_url[index] = permalink_urls["tracks"][index]["permalink_url"]
 		trackid[index] = permalink_urls["tracks"][index]["id"]
 		track_title[index] = permalink_urls["tracks"][index]["title"]
-		#print index + 1, "\t", trackid[index], "\t", permalink_url[index]
 
-	return permalink_url, trackid, track_title
+	return permalink_url, trackid, track_title, album
 
-def download_track(trackid, song_url, track_title):
+def download_playlist(soundcloud_url):
+	playlist_id = get_playlist_id(soundcloud_url)
+	print "Playlist ID:", playlist_id
+	permalink_url, trackid, track_name, album = get_playlist_tracks(playlist_id)
+
+	track_name = []
+	artist = []
+	coverflag = []
+	cover_file = []
+
+	for index in range(len(trackid)):
+		track_name.append(index)
+		artist.append(index)
+		coverflag.append(index)
+		cover_file.append(index)
+		track_name[index], artist[index], coverflag[index], cover_file[index] = get_tags(permalink_url[index])
+		print u'\r[{}]/[{}] \t{}'.format(index + 1, max(range(len(trackid))) + 1, track_name[index], track_name[index])
+		download_track(trackid[index], permalink_url[index], track_name[index]),
+		print u"\r                                                    "
+		#go to line above
+		sys.stdout.write("\033[F")
+		sys.stdout.flush()
+		add_tags(track_name[index], artist[index], cover_file[index], album)
+
+	print "Done!"
+
+def add_tags(track_name, artist, cover_file, album):
+	try:
+		audio = EasyID3(u"%s" % track_name + u".mp3")
+	except mutagen.id3._util.ID3NoHeaderError:
+		audio = mutagen.File(u"%s" % track_name + u".mp3", easy=True)
+		audio.add_tags()
+	audio['title'] = u"%s" % track_name
+	audio['artist'] = u"%s" % artist
+	if album is not 0:
+		audio['album'] = u"%s" % album
+	else:
+		audio['album'] = u"SoundCloud"
+	if cover_file is not 0:
+	    with tempfile.NamedTemporaryFile() as out_file:
+	    	shutil.copyfileobj(cover_file, out_file)
+	        out_file.seek(0)
+
+		audio.save()
+		audio = MP3(u"%s" % track_name + u".mp3", ID3=ID3)
+		audio.tags.add(
+			APIC(
+				encoding=3,
+				mime='image/jpeg',
+				type=3,
+				desc=u'Cover',
+				data=out_file.read()
+				)
+			)
+		audio.save()
+
+def download_track(trackid, song_url, track_name):
 	trackid = str(trackid)
 	song_url = str(song_url)
 	client_id = "c8ce5cbca9160b790311f06638a61037"
@@ -80,9 +195,9 @@ def download_track(trackid, song_url, track_title):
 	file_mp3_url = file_mp3_url.replace("\u0026", "&")
 
 	#Download the track
-	filename = u"%s" % track_title + ".mp3"
+	filename = (u"%s" % track_name) + u".mp3"
 	if not os.path.exists(filename):
-		with open(filename, "wb") as file:
+		with open(unicode(filename), "wb") as file:
 			filedl = requests.get(file_mp3_url, stream=True)
 			file_length = filedl.headers.get('content-length')
 			file_length = int(file_length)
@@ -98,82 +213,77 @@ def download_track(trackid, song_url, track_title):
 	else:
 		pass
 
+def download_user_tracks(soundcloud_url):
+	user_id = get_user_id(soundcloud_url)
+	print "User ID:", user_id
+	track_ids, permalink_url, track_name, album = get_user_tracks(user_id)
+
+	track_name = []
+	artist = []
+	coverflag = []
+	cover_file = []
+
+	for index in range(len(track_ids)):
+		track_name.append(index)
+		artist.append(index)
+		coverflag.append(index)
+		cover_file.append(index)
+		track_name[index], artist[index], coverflag[index], cover_file[index] = get_tags(permalink_url[index])
+		print u'\r[{}]/[{}] \t{}'.format(index + 1, max(range(len(track_ids))) + 1, track_name[index], track_name[index])
+		download_track(track_ids[index], permalink_url[index], track_name[index]),
+		print u"\r                                                    "
+		#go to line above
+		sys.stdout.write("\033[F")
+		sys.stdout.flush()
+		add_tags(track_name[index], artist[index], cover_file[index], album)
+
+
+
 def get_tags(soundcloud_url):
 	client_id = "fDoItMDbsbZz8dY16ZzARCZmzgHBPotA"
 	resolve_url = "https://api.soundcloud.com/resolve.json?url="
 	a = requests.get(resolve_url + soundcloud_url + "&client_id=" + client_id)
 	a = a.content
 	tags = json.loads(a)
-	track_name = tags["title"]
+	track_name = u"%s" % tags["title"]
+	if u"/" in track_name:
+		track_name = track_name.replace(u"/", u"-")
 	
-	artist = str(tags["user"]["username"])
+	artist = u"%s" % tags["user"]["username"]
 	cover = tags["artwork_url"]
-	if cover:
+	if cover is not None:
 		coverflag = 1
 		cover = cover.replace("large", "t500x500")
-		cover_download = requests.get(cover)
-		open('cover.jpg', 'w').write(cover_download.content)
+		cover_request = requests.get(cover, stream=True)
+		cover_file = cover_request.raw
+		#cover_download = requests.get(cover)
+		#open('cover.jpg', 'w').write(cover_download.content)
 	else:
 		coverflag = 0
 
-	return track_name, artist, coverflag
+	if (coverflag == 0):
+		cover_file = 0
+		return  track_name, artist, coverflag, cover_file
+	return track_name, artist, coverflag, cover_file
 
-def get_album_name(soundcloud_url):
-	client_id = "fDoItMDbsbZz8dY16ZzARCZmzgHBPotA"
-	resolve_url = "https://api.soundcloud.com/resolve.json?url="
-	a = requests.get(resolve_url + soundcloud_url + "&client_id=" + client_id)
-	a = a.content
-	tags = json.loads(a)
-	album = tags["title"]
-	return album
-
-
-def add_tags(title, artist, coverflag):
-	try:
-		audio = EasyID3("%s" % title + ".mp3")
-	except mutagen.id3._util.ID3NoHeaderError:
-		audio = mutagen.File("%s" % title + ".mp3", easy=True)
-		audio.add_tags()
-	audio['title'] = u"%s" % title
-	audio['artist'] = u"%s" % artist
-	if '/sets/' in soundcloud_url:
-		audio['album'] = u"%s" % album
-	else:
-		audio['album'] = u"SoundCloud"
-
-	audio.save()
-	audio = MP3("%s" % title + ".mp3", ID3=ID3)
-	if coverflag == 1:
-		audio.tags.add(
-			APIC(
-				encoding=3,
-				mime='image/jpeg',
-				type=3,
-				desc=u'Cover',
-				data=open('cover.jpg').read()
-				)
-			)
-	audio.save()
-
-def delete_albumart():
-	os.remove("cover.jpg")
-
-def get_user_id (profile_url):
-	url = "https://api-mobi.soundcloud.com/resolve?permalink_url=" + profile_url + "&client_id=c8ce5cbca9160b790311f06638a61037&format=json&app_version=1481130054"
+def get_user_id(soundcloud_url):
+	url = u"https://api-mobi.soundcloud.com/resolve?permalink_url=" + soundcloud_url + u"&client_id=c8ce5cbca9160b790311f06638a61037&format=json&app_version=1481130054"
 	user_id = requests.get(url)
-	#print user_id.content
 	user_id = json.loads(user_id.content)
 	user_id = user_id["id"]
 	return user_id
 
 def get_user_tracks(user_id):
-	url = "https://api-v2.soundcloud.com/users/" + str(user_id) + "/tracks?representation=&client_id=fDoItMDbsbZz8dY16ZzARCZmzgHBPotA&limit=5000&offset=0&linked_partitioning=1&app_version=1486402392"
+	offset = 0
+	url = u"https://api-v2.soundcloud.com/users/" + unicode(user_id) + u"/tracks?representation=&client_id=fDoItMDbsbZz8dY16ZzARCZmzgHBPotA&limit=5000&offset=0&linked_partitioning=1&app_version=1486402392"
 	playlist = requests.get(url)
 	playlist = playlist.content
 	playlist = json.loads(playlist)
+	album = playlist["collection"][0]["user"]["full_name"]
 	track_ids = []
 	permalink_url = []
 	track_name = []
+
 	for index in range(len(playlist["collection"])):
 		track_ids.append(index)
 		permalink_url.append(index)
@@ -181,91 +291,42 @@ def get_user_tracks(user_id):
 		track_ids[index] = playlist["collection"][index]["id"]
 		permalink_url[index] = playlist["collection"][index]["permalink_url"]
 		track_name[index] = playlist["collection"][index]["title"]
-	return track_ids, permalink_url, track_name
 
+	try:
+		if playlist["next_href"] is not None:
+			track_ids, permalink_url, track_name = get_user_tracks_recursion(playlist["next_href"], url, track_ids, permalink_url, track_name)
+	except:
+		return track_ids, permalink_url, track_name, album
 
-#Hide the cursor
-try:
-	sys.stdout.write("\033[?25l")
-	sys.stdout.flush()
-	#fix URL if it is a single track out of a playlist
-	if '?in=' in soundcloud_url:
-		soundcloud_url = soundcloud_url.split('?in=')
-		soundcloud_url = soundcloud_url[0]
+def get_user_tracks_recursion(next_href, first_url, track_ids, permalink_url, track_name):
+	link_to_next_part = next_href.split("offset=")
+	link_to_next_part = link_to_next_part[1].split("&representation=")
+	link_to_next_part = link_to_next_part[0]
+	link_to_next_part = first_url.replace("offset=0", "offset=" + str(link_to_next_part))
+	next_part = requests.get(link_to_next_part)
+	next_part = next_part.content
+	next_part = json.loads(next_part)
 
-	#check for pattern in url, if it is a user page or a track page of a user
-	user_re = re.compile(r"^https:\/\/soundcloud.com\/[abcdefghijklmnopqrstuvwxyz\-_1234567890]{1,}$")
-	user_re_tracks = re.compile(r"^https:\/\/soundcloud.com\/[abcdefghijklmnopqrstuvwxyz\-_1234567890]{1,25}\/tracks$")
-	if (user_re.match(soundcloud_url)) or (user_re_tracks.match(soundcloud_url)):
-		user_id = get_user_id(soundcloud_url)
-		track_ids, permalink_url, track_title = get_user_tracks(user_id)
-		for index in range(len(track_ids)):
+	for index in range(len(next_part["collection"])):
+		track_ids.append(next_part["collection"][index]["id"])
+		permalink_url.append(next_part["collection"][index]["permalink_url"])
+		track_name.append(next_part["collection"][index]["title"])
 
-			if "/" in track_title[index]:
-				track_title[index] = track_title[index].replace("/", "-")
+	if next_part["next_href"]:
+		get_user_tracks_recursion(next_part["next_href"], first_url, track_ids, permalink_url, track_name)
 
-			print '\r[{}]/[{}] \t{}'.format(index + 1, max(range(len(track_ids))) + 1, track_title[index], track_title[index])
-			download_track(track_ids[index], permalink_url[index], track_title[index]),
-			print "\r                                                    "
-			#go to line above
-			sys.stdout.write("\033[F")
-			sys.stdout.flush() 
-			track_name, artist, coverflag = get_tags(permalink_url[index])
-			if "/" in track_name:
-				track_name = track_name.replace("/", "-")
+	else:
+		return track_ids, permalink_url, track_name
 
-			album = artist
-			add_tags(track_name, artist, coverflag)
-			if coverflag == 1:
-				delete_albumart()
+def get_track_id(soundcloud_url):
+	client_id = u"fDoItMDbsbZz8dY16ZzARCZmzgHBPotA"
+	resolve_url = u"https://api.soundcloud.com/resolve.json?url="
+	url = resolve_url + soundcloud_url + u"&client_id=" + client_id
+	s = requests.get(resolve_url + soundcloud_url + u"&client_id=" + client_id)
+	s = s.content
+	trackid = json.loads(s)
+	trackid = trackid["id"]
+	return trackid
 
-		print "Done!"
-		#Show cursor again
-		sys.stdout.write("\033[?25h")
-		sys.stdout.flush()
-
-	# Checks, whether the URL is a playlist or not
-	if '/sets' in soundcloud_url:
-		permalink_url, trackid, track_title = get_trackids_playlist(soundcloud_url)
-
-		for index in range(len(trackid)):
-			if "/" in track_title[index]:
-				track_title[index] = track_title[index].replace("/", "-")
-			print '\r[{}]/[{}] \t{}'.format(index + 1, max(range(len(trackid))) + 1, track_title[index], track_title[index])
-			download_track(trackid[index], permalink_url[index], track_title[index]),
-			print "\r                                                    "
-			#go to line above
-			sys.stdout.write("\033[F")
-			sys.stdout.flush() 
-			track_name, artist, coverflag = get_tags(permalink_url[index])
-			if "/" in track_name:
-				track_name = track_name.replace("/", "-")
-			album = get_album_name(soundcloud_url)
-			add_tags(track_name, artist, coverflag)
-			if coverflag == 1:
-				delete_albumart()
-
-		print "Done!"
-		#Show cursor again
-		sys.stdout.write("\033[?25h")
-		sys.stdout.flush()
-
-
-	else:	
-		track_name, trackid = get_trackid_single_song(soundcloud_url)
-		if "/" in track_name:
-				track_name = track_name.replace("/", "-")
-		download_track(trackid, soundcloud_url, track_name);
-		track_name, artist, coverflag = get_tags(soundcloud_url)
-		add_tags(track_name, artist, coverflag)
-		if coverflag == 1:
-			delete_albumart()
-		print "\nDone!"
-		#Show cursor again
-		sys.stdout.write("\033[?25h")
-		sys.stdout.flush()
-
-except:
-	#print "\nAborting..."
-	sys.stdout.write("\033[?25h")
-	sys.stdout.flush()
+if __name__ == '__main__':
+    main()
